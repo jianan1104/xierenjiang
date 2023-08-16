c27ae3940e5401ac36dedf6b77d3b1822c67a418
Kane <kane@KanedeMacBook-Pro.local>
2023-08-16 22:48:14 +0800

    fix broken link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,7 +105,7 @@ const config = {
               },
               {
                 label: 'Portfolio',
-                to: '/portfolio',
+                to: '/docs/category/media-reports',
               },
             ],
           },
